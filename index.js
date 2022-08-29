4b1e5f27b120a1362e4176581e5200a42e8fa034
RicholeYu <richole.yu@qq.com>
2022-08-29 18:05:44 +0800

    .

--- index.js
+++ index.js
@@ -1,51 +1,81 @@
 #!/usr/bin/env node
+const fetch = require('node-fetch')
 const http = require("http")
+const arg = require('arg')
+const sound = require('@dfyu/sound')
 const log = console.log
 const craeteSeparation = () => log(new Array(60).fill("=").join(""))
-let words = process.argv.slice(2)
 
-if (words.length > 0) {
-    if (words.filter(item => item === "-s").length >= 1) {
-        words = [words.filter(item => item !== "-s").join(" ")]
-    }
-    words.forEach(word => {
-        const url = `http://fanyi.youdao.com/openapi.do?keyfrom=richole&key=1196902348&type=data&doctype=json&version=1.1&q=${encodeURIComponent(word)}`
-        let thisChunk = ""
-        const thisRequest = http.get(new URL(url), {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }, res => {
-            res.setEncoding("utf-8")
-
-            res.on("data", chunk => {
-                thisChunk += chunk.toString()
-            })
-
-            res.on("end", () => {
-                const thisData = JSON.parse(thisChunk)
-                craeteSeparation()
-                log(word)
-                log(thisData.translation.join("  "))
-                if (thisData.basic && thisData.basic.explains) {
-                    log(thisData.basic.explains.join("\n"))
-                }
-                if (thisData.web) {
-                    thisData.web.forEach((item) => {
-                        log(`${item.key}: ${item.value.join(",")}`)
-                    })
-                }
-                craeteSeparation()
-            })
-        })
-        thisRequest.on("error", e => {
-            log(`error: ${e.toString()}`)
-            craeteSeparation()
-        })
-        thisRequest.end()
-    })
-} else {
-    log("请输入待翻译词语，多个词语请用空格隔开, 若翻译句子加上参数 -s")
-    log("例如: translate hello")
-    log("例如: translate -s hello world")
+let args = null
+try {
+  args = arg({
+    '-s': Boolean,
+    '--s': Boolean
+  });
+} catch (err) {
+  console.log(err.message);
+  process.exit(0)
 }
+
+const isSentence = !!args['-s']
+const openSound = !!args['--s']
+const words = args['_']
+
+async function translate (string) {
+  const data = await fetch("https://dict.youdao.com/jsonapi_s?doctype=json&jsonversion=4", {
+    "headers": {
+      "accept": "application/json, text/plain, */*",
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    "body": `q=${encodeURIComponent(string)}&le=en&t=1&client=web&keyfrom=webdict`,
+    "method": "POST",
+  }).then(res => res.json())
+  const result = data?.fanyi?.tran || data?.ce?.word?.trs?.[0]?.['#text'] || data?.ec?.word?.trs?.[0]?.tran
+  console.log(result)
+
+  return result
+}
+
+(async () => {
+  const reg = /^[a-zA-Z,.\s]+$/
+
+  if (args && words.length > 0) {
+    if (isSentence) {
+      const string = words.join(' ')
+      const result = await translate(string)
+
+      if (openSound) {
+        if (reg.test(string)) {
+          await sound(string)
+        } else if (reg.test(result)) {
+          await sound(result)
+        }
+      }
+    } else {
+      craeteSeparation()
+      for await (let word of words) {
+        console.log(word)
+        const result = await translate(word)
+
+        if (openSound) {
+          if (reg.test(word)) {
+            await sound(word)
+          } else if (reg.test(result)) {
+            await sound(result)
+          }
+        }
+
+        craeteSeparation()
+      }
+    }
+  } else {
+    log("请输入待翻译词语，多个词语请用空格隔开, 若翻译句子加上参数 -s, 若需要朗读翻译加上参数 --s")
+    log("例如: translate hello")
+    log("例如: translate hello --s")
+    log("例如: translate hello world")
+    log("例如: translate hello world --s")
+    log("例如: translate -s \"hello world\"")
+    log("例如: translate -s \"hello world\" --s")
+  }
+})();
+
